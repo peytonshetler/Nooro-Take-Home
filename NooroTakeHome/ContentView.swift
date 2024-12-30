f0e28f02946044e8172ddff7b76eced4412dd08f
Peyton Shetler <peyton.shetler@gmail.com>
2024-12-30 17:46:03 -0600

    Initial Commit

---
+++ NooroTakeHome/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NooroTakeHome
+//
+//  Created by Peyton Shetler on 12/30/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NooroTakeHome/NooroTakeHomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  NooroTakeHomeApp.swift
+//  NooroTakeHome
+//
+//  Created by Peyton Shetler on 12/30/24.
+//
+
+import SwiftUI
+
+@main
+struct NooroTakeHomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
